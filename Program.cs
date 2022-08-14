4a9728ca702b201f15de687f3877d08de9813b46
goya123 <akhilg642@gmail.com>
2022-08-14 14:55:44 +0530

    Validating LastName via regex

--- Program.cs
+++ Program.cs
@@ -6,17 +6,30 @@ namespace UserRegistration
     {
         public static void Main(string[] args)
         {
-            String FirstName;
+            String firstName,lastName;
             Console.WriteLine("Welcome to the Regex Implementation");
-            
+
             Regex regex = new Regex("^([A-Z]{1}[a-z]{1}[a-z]{1}([a-z]+))$");
 
-            FirstName = Console.ReadLine();
-            Match match=regex.Match(FirstName);
-            if (match.Success)
-                Console.WriteLine("match.value = " + match.Value);
+            Console.WriteLine("Enter First Name");
+            firstName = Console.ReadLine();
 
-            
+            Match matchfname=regex.Match(firstName);
+            if (matchfname.Success)
+                Console.WriteLine("match.value = " + matchfname.Value);
+            else
+                Console.WriteLine("Not Correct");
+
+            Console.WriteLine("Enter Last Name");
+            lastName = Console.ReadLine();
+
+            Match matchlname = regex.Match(lastName);
+            if (matchlname.Success)
+                Console.WriteLine("match.value = " + matchlname.Value);
+            else
+                Console.WriteLine("Not Correct");
+
+
         }
     }
 }
