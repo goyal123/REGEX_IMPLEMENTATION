5ced7f9b5daf2000f9e30f73ca0b92b4414d98d1
goya123 <akhilg642@gmail.com>
2022-08-14 16:19:44 +0530

    Mobile number validation

--- Program.cs
+++ Program.cs
@@ -6,11 +6,15 @@ namespace UserRegistration
     {
         public static void Main(string[] args)
         {
-            String firstName,lastName,email;
+            String firstName,lastName,email,phoneNumber;
+            
             Console.WriteLine("Welcome to the Regex Implementation");
 
             Regex nameregex = new Regex("^([A-Z]{1}[a-z]{1}[a-z]{1}([a-z]+)?)$");
             Regex emailregex = new Regex("^([^.][a-zA-Z0-9\\-]+)(.[a-z0-9]+)?@([a-zA-Z0-9\\-]+)\\.([a-z]{2,4})((.[a-z]{2,4})?)$");
+            Regex mobileregex = new Regex("^([1-9]{2})(\\s)([0-9]{10})$");
+
+
 
             Console.WriteLine("Enter First Name");
             firstName = Console.ReadLine();
@@ -37,6 +41,15 @@ namespace UserRegistration
             Match matchemail = emailregex.Match(email);
             if (matchemail.Success)
                 Console.WriteLine("match.value = " + matchemail.Value);
+            else
+                Console.WriteLine("Not Correct");
+
+            Console.WriteLine("Enter Mobile Number");
+            phoneNumber =Console.ReadLine();
+
+            Match matchmobile = mobileregex.Match(phoneNumber);
+            if (matchmobile.Success)
+                Console.WriteLine("match.value = " + matchmobile.Value);
             else
                 Console.WriteLine("Not Correct");
 
