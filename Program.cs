534859d86bb5d80cb144343303e9355dccb8a7dc
goya123 <akhilg642@gmail.com>
2022-08-14 14:37:35 +0530

    Project Dir created with welcome message on screen

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Regex Implementation");
+        }
+    }
+}
