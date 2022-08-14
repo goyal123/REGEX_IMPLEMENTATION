c0f32976b90f4bfb15393c82e21059c091aa9894
goya123 <akhilg642@gmail.com>
2022-08-14 14:49:17 +0530

    Validated FirstName via regex

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace UserRegistration
+﻿using System.Text.RegularExpressions;
+
+namespace UserRegistration
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            String FirstName;
             Console.WriteLine("Welcome to the Regex Implementation");
+            
+            Regex regex = new Regex("^([A-Z]{1}[a-z]{1}[a-z]{1}([a-z]+))$");
+
+            FirstName = Console.ReadLine();
+            Match match=regex.Match(FirstName);
+            if (match.Success)
+                Console.WriteLine("match.value = " + match.Value);
+
+            
         }
     }
 }
