cc9ca5a98cd10476a5dc3f4c2797ab7daa888bc0
goya123 <akhilg642@gmail.com>
2022-08-14 16:36:24 +0530

    Password validation

--- Program.cs
+++ Program.cs
@@ -6,16 +6,17 @@ namespace UserRegistration
     {
         public static void Main(string[] args)
         {
-            String firstName,lastName,email,phoneNumber;
+            String firstName,lastName,email,phoneNumber,password;
             
             Console.WriteLine("Welcome to the Regex Implementation");
 
-            Regex nameregex = new Regex("^([A-Z]{1}[a-z]{1}[a-z]{1}([a-z]+)?)$");
-            Regex emailregex = new Regex("^([^.][a-zA-Z0-9\\-]+)(.[a-z0-9]+)?@([a-zA-Z0-9\\-]+)\\.([a-z]{2,4})((.[a-z]{2,4})?)$");
-            Regex mobileregex = new Regex("^([1-9]{2})(\\s)([0-9]{10})$");
+            //Regex nameregex = new Regex("^([A-Z]{1}[a-z]{1}[a-z]{1}([a-z]+)?)$");
+            //Regex emailregex = new Regex("^([^.][a-zA-Z0-9\\-]+)(.[a-z0-9]+)?@([a-zA-Z0-9\\-]+)\\.([a-z]{2,4})((.[a-z]{2,4})?)$");
+            //Regex mobileregex = new Regex("^([1-9]{2})(\\s)([0-9]{10})$");
+            Regex passwdregex = new Regex("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
 
 
-
+            /*
             Console.WriteLine("Enter First Name");
             firstName = Console.ReadLine();
 
@@ -50,6 +51,16 @@ namespace UserRegistration
             Match matchmobile = mobileregex.Match(phoneNumber);
             if (matchmobile.Success)
                 Console.WriteLine("match.value = " + matchmobile.Value);
+            else
+                Console.WriteLine("Not Correct");
+            */
+
+            Console.WriteLine("Enter password");
+            password = Console.ReadLine();
+
+            Match matchpasswd = passwdregex.Match(password);
+            if (matchpasswd.Success)
+                Console.WriteLine("match.value = " + matchpasswd.Value);
             else
                 Console.WriteLine("Not Correct");
 
