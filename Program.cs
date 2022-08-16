1e5309066b5589a5d1ab16cea8b07107fcae9942
goya123 <akhilg642@gmail.com>
2022-08-16 20:12:55 +0530

    Modyifing program.cs

--- Program.cs
+++ Program.cs
@@ -6,62 +6,7 @@ namespace UserRegistration
     {
         public static void Main(string[] args)
         {
-            String firstName,lastName,email,phoneNumber,password;
-            
-            Console.WriteLine("Welcome to the Regex Implementation");
-
-            Regex nameregex = new Regex("^([A-Z]{1}[a-z]{1}[a-z]{1}([a-z]+)?)$");
-            Regex emailregex = new Regex("^([^.][a-zA-Z0-9\\-]+)(.[a-z0-9]+)?@([a-zA-Z0-9\\-]+)\\.([a-z]{2,4})((.[a-z]{2,4})?)$");
-            Regex mobileregex = new Regex("^([1-9]{2})(\\s)([0-9]{10})$");
-            Regex passwdregex = new Regex("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
-
-            Console.WriteLine("Enter First Name");
-            firstName = Console.ReadLine();
-
-            Match matchfname=nameregex.Match(firstName);
-            if (matchfname.Success)
-                Console.WriteLine("match.value = " + matchfname.Value);
-            else
-                Console.WriteLine("Not Correct");
-
-            Console.WriteLine("Enter Last Name");
-            lastName = Console.ReadLine();
-
-            Match matchlname = nameregex.Match(lastName);
-            if (matchlname.Success)
-                Console.WriteLine("match.value = " + matchlname.Value);
-            else
-                Console.WriteLine("Not Correct");
-
-
-            Console.WriteLine("Enter Email");
-            email = Console.ReadLine();
-
-            Match matchemail = emailregex.Match(email);
-            if (matchemail.Success)
-                Console.WriteLine("match.value = " + matchemail.Value);
-            else
-                Console.WriteLine("Not Correct");
-
-            Console.WriteLine("Enter Mobile Number");
-            phoneNumber =Console.ReadLine();
-
-            Match matchmobile = mobileregex.Match(phoneNumber);
-            if (matchmobile.Success)
-                Console.WriteLine("match.value = " + matchmobile.Value);
-            else
-                Console.WriteLine("Not Correct");
-            
-
-            Console.WriteLine("Enter password");
-            password = Console.ReadLine();
-
-            Match matchpasswd = passwdregex.Match(password);
-            if (matchpasswd.Success)
-                Console.WriteLine("match.value = " + matchpasswd.Value);
-            else
-                Console.WriteLine("Not Correct");
-
+            Person.AddPerson();
         }
     }
 }
